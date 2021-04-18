a1d1f97f909f20bbbe29871fd975f2ec8f1d2d82
buaaymh <yangminghao@buaa.edu.cn>
2021-04-18 20:02:41 +0800

    Add Integrate for cell.

--- include/buaa/element/triangle.hpp
+++ include/buaa/element/triangle.hpp
@@ -2,44 +2,45 @@
 #ifndef INCLUDE_BUAA_ELEMENT_TRIANGLE_HPP_
 #define INCLUDE_BUAA_ELEMENT_TRIANGLE_HPP_
 
+#include <array>
 #include <cmath>
 #include <initializer_list>
-#include <vector>
 
 #include <Eigen/Dense>
 
 #include "buaa/element/edge.hpp"
 #include "buaa/element/node.hpp"
+#include "buaa/element/data.hpp"
 
 namespace buaa {
 namespace element {
 
-template <class EdgeData, class CellData>
 class Edge;
 
-template <int kDim, class EdgeData, class CellData>
 class Triangle {
  public:
   // Types:
-  using Edge = element::Edge<EdgeData, CellData>;
-  using Node = typename Edge::NodeType;
-  using Point = typename Edge::PointType;;
-  using Data = CellData;
+  using EdgeType = Edge;
+  using NodeType = Node<2>;
+  using PointType = Point<2>;
+  using Matrix3 = Matrix<Scalar, 3, 3>;
+  using Vector3 = Matrix<Scalar, 3, 1>;
   // Constructors:
   Triangle() = default;
-  Triangle(const Node& a, const Node& b, const Node& c,
-      std::initializer_list<Edge*> edges) : a_(a), b_(b), c_(c), edges_{edges} {
-      auto cross = (b_.X() - a_.X()) * (c_.Y() - a_.Y()) -
-                   (b_.Y() - a_.Y()) * (c_.X() - a_.X());   
-      measure_ = std::abs(cross) * 0.5;
-      center_ = Point((a + b + c) / 3);
+  Triangle(Id id, const NodeType& a, const NodeType& b, const NodeType& c,
+      std::initializer_list<EdgeType*> edges) : id_(id), a_(a), b_(b), c_(c), 
+      edges_{*edges.begin(), *(edges.begin()+1), *(edges.begin()+2)} { 
+      measure_ = GetMeasure(a, b, c);
+      center_ = PointType((a + b + c) / 3);
+      mat_ << 1.0, 1.0, 1.0, a.X(), b.X(), c.X(), a.Y(), b.Y(), c.Y();
   }
   // Accessors:
+  Id I() const { return id_; }
   int CountVertices() const { return 3; }
-  const Node& A() const { return a_; }
-  const Node& B() const { return b_; }
-  const Node& C() const { return c_; }
-  const Node& GetNode(int i) const {
+  const NodeType& A() const { return a_; }
+  const NodeType& B() const { return b_; }
+  const NodeType& C() const { return c_; }
+  const NodeType& GetNode(int i) const {
     switch (i)  {
     case 0:
       return A();
@@ -51,19 +52,48 @@ class Triangle {
       throw std::out_of_range("A `Triangle` has 3 `Point`s.");
     }
   }
+  // Integrator:
+  PointType GetGlobalXY(Scalar a, Scalar b, Scalar c) const {
+    Vector3 abc = Vector3{a, b, c};
+    Vector3 xy = mat_ * (abc);
+    return PointType(xy(1), xy(2));
+  }
+  template <class Integrand>
+  Scalar Integrate(Integrand&& integrand) const {
+    Scalar result = 0.0;
+    for (int i = 0; i < 4; ++i) {
+      auto point = GetGlobalXY(local_a[i], local_b[i], local_c[i]);
+      result += integrand(point.X(), point.Y()) * weights[i];
+    }
+    return result *= Measure();
+  }
   // Geometric methods:
-  Scalar Measure() const {return measure_; }
-  const Point& Center() const { return center_; }
-  // Data:
-  Data data;
+  Scalar Measure() const { return measure_; }
+  const PointType& Center() const { return center_; }
 
  private:
-  const Node& a_;
-  const Node& b_;
-  const Node& c_;
-  std::vector<Edge*> edges_;
+  Scalar GetMeasure(const NodeType& a, const NodeType& b, const NodeType& c) {
+    auto cross = (b.X() - a.X()) * (c.Y() - a.Y()) -
+                 (b.Y() - a.Y()) * (c.X() - a.X());   
+    return std::abs(cross) * 0.5;
+  }
+ private:
+  static constexpr std::array<Scalar, 4> local_a{0.3333333333333333, 0.6, 0.2, 0.2};
+  static constexpr std::array<Scalar, 4> local_b{0.3333333333333333, 0.2, 0.2, 0.6};
+  static constexpr std::array<Scalar, 4> local_c{0.3333333333333333, 0.2, 0.6, 0.2};
+  static constexpr std::array<Scalar, 4> weights{-0.5624999999999998,
+                                                  0.5208333333333332,
+                                                  0.5208333333333332,
+                                                  0.5208333333333332};
+ private:
+  Id id_;
+  const NodeType& a_;
+  const NodeType& b_;
+  const NodeType& c_;
+  std::array<Edge*, 3> edges_;
   Scalar measure_;
-  Point center_;
+  PointType center_;
+  Matrix3 mat_;
 };
 
 }  // namespace element
--- test/element/triangle.cpp
+++ test/element/triangle.cpp
@@ -1,6 +1,6 @@
 // Copyright 2021 Weicheng Pei and Minghao Yang
 #include "buaa/element/triangle.hpp"
-#include "buaa/mesh/data.hpp"
+#include "buaa/element/data.hpp"
 
 #include "gtest/gtest.h"
 
@@ -9,30 +9,59 @@ namespace element {
 
 class TriangleTest : public ::testing::Test {
  protected:
-  using TriangleType = Triangle<2, mesh::Empty, Scalar>;
-  using EdgeType = TriangleType::Edge;
-  using NodeType = TriangleType::Node;
-  NodeType a{0, 0.0, 0.0}, b{1, 1.0, 0.0}, c{2, 0.0, 1.0};
+  using TriangleType = Triangle;
+  using EdgeType = TriangleType::EdgeType;
+  using NodeType = TriangleType::NodeType;
+  using PointType = TriangleType::PointType;
+  NodeType a{0, 0.0, 0.0}, b{1, 3.0, 0.0}, c{2, 0.0, 3.0};
   EdgeType ab{a, b}, bc{b, c}, ca{c, a};
+  Id id{1};
 };
 TEST_F(TriangleTest, TemplateConstructor) {
-  auto cell = TriangleType(a, b, c, {&ab, &bc, &ca});
+  auto cell = TriangleType(id, a, b, c, {&ab, &bc, &ca});
+  EXPECT_EQ(cell.I(), id);
   EXPECT_EQ(cell.CountVertices(), 3);
 }
 TEST_F(TriangleTest, GeometricMethods) {
-  auto cell = TriangleType(a, b, c, {&ab, &bc, &ca});
+  auto cell = TriangleType(id, a, b, c, {&ab, &bc, &ca});
+  EXPECT_EQ(cell.I(), id);
   EXPECT_EQ(cell.A().X(), a.X());
   EXPECT_EQ(cell.GetNode(0).X(), a.X());
   EXPECT_EQ(cell.B().X(), b.X());
   EXPECT_EQ(cell.GetNode(1).X(), b.X());
   EXPECT_EQ(cell.C().X(), c.X());
   EXPECT_EQ(cell.GetNode(2).X(), c.X());
-  EXPECT_EQ(cell.Measure(), 0.5);
-  auto node = cell.Center();
-  EXPECT_EQ(node.X() * 3, a.X() + b.X() + c.X());
-  EXPECT_EQ(node.Y() * 3, a.Y() + b.Y() + c.Y());
-  cell.data = cell.Measure();
-  EXPECT_EQ(cell.data, 0.5);
+  EXPECT_EQ(cell.Measure(), 4.5);
+  EXPECT_EQ(cell.Center().X(), 1.0);
+  EXPECT_EQ(cell.Center().Y(), 1.0);
+}
+TEST_F(TriangleTest, LocalToGlobalXY) {
+  auto cell = TriangleType(id, a, b, c, {&ab, &bc, &ca});
+  auto global_p = cell.GetGlobalXY(1.0/3, 1.0/3, 1.0/3);
+  EXPECT_EQ(global_p.X(), cell.Center().X());
+  EXPECT_EQ(global_p.Y(), cell.Center().Y());
+  auto global_a = cell.GetGlobalXY(1.0, 0.0, 0.0);
+  EXPECT_EQ(global_a.X(), a.X());
+  EXPECT_EQ(global_a.Y(), a.Y());
+  auto global_b = cell.GetGlobalXY(0.0, 1.0, 0.0);
+  EXPECT_EQ(global_b.X(), b.X());
+  EXPECT_EQ(global_b.Y(), b.Y());
+  auto global_c = cell.GetGlobalXY(0.0, 0.0, 1.0);
+  EXPECT_EQ(global_c.X(), c.X());
+  EXPECT_EQ(global_c.Y(), c.Y());
+}
+TEST_F(TriangleTest, Integrator) {
+  auto cell = TriangleType(id, a, b, c, {&ab, &bc, &ca});
+  auto integrand_0 = [](auto x, auto y) { return 3.14; };
+  EXPECT_DOUBLE_EQ(cell.Integrate(integrand_0), cell.Measure() * 3.14);
+  auto integrand_1 = [](auto x, auto y) { return x - 1.0; };
+  EXPECT_DOUBLE_EQ(cell.Integrate(integrand_1), 0.0);
+  auto integrand_2 = [](auto x, auto y) { return y - 1.0; };
+  EXPECT_DOUBLE_EQ(cell.Integrate(integrand_2), 0.0);
+  auto integrand_3 = [](auto x, auto y) { return x * y; };
+  EXPECT_DOUBLE_EQ(cell.Integrate(integrand_3), 3.375);
+  auto integrand_4 = [](auto x, auto y) { return x * x * y; };
+  EXPECT_DOUBLE_EQ(cell.Integrate(integrand_4), 4.05);
 }
 
 }  // namespace element
